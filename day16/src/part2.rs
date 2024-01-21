90c19d8136073a0d88c9a967de5cde2dca4dea12
Thijs Oostdam <thijs.oostdam@nn.nl>
2024-01-21 10:54:37 +0100

    day 17, has a bug somewhere that only shows up on part 2..

--- day16/src/part2.rs
+++ day16/src/part2.rs
@@ -1,2 +1,259 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+use grid::grid;
+use grid::Grid;
+use itertools::Itertools;
+
 pub(crate) fn part2(file: &String) {
+  let contraption = contraption(file);
+
+  let mut start_states = vec![];
+
+  for c in 0..contraption.cols(){
+    start_states.push(State{row: 0, col: c, direction: Direction::DOWN});
+    start_states.push(State{row: contraption.rows()-1, col: c, direction: Direction::UP});
+  }
+  for r in 0..contraption.rows(){
+    start_states.push(State{row: r, col: 0, direction: Direction::RIGHT});
+    start_states.push(State{row: r, col: contraption.cols()-1, direction: Direction::LEFT});
+  }
+
+  let answer = start_states.into_iter().map(|state|energized(state, &contraption)).max().unwrap();
+
+  println!("{answer}");
+}
+
+fn print_grid(grid: &Grid<char>) {
+  for row in grid.iter_rows() {
+    for c in row {
+      print!("{c}");
+    }
+    println!();
+  }
+}
+
+fn energized(start_state: State, contraption: &Grid<char>)->usize{
+
+  // approach as a state problem
+  // state is beam direction + location
+  // solve until no more states exist.
+
+  // next state is applying beam direction to contraption location
+  // mirror = split or passthrough
+  // empty = passthrough
+  // out of bounds is no new state.
+
+  // store each new state in a set
+  // if state already exists in set, don't add it to the stack.
+
+  let mut states = VecDeque::<State>::new();
+  let mut visited = HashSet::<State>::new();
+  states.push_front(start_state);
+
+  while let Some(state) = states.pop_back() {
+    let next = resolve(&state, &contraption);
+    next.into_iter().for_each(|s|{
+      if !visited.contains(&s){
+        states.push_back(s);
+      }
+    });
+    visited.insert(state);
+  }
+
+  let mut v = Grid::new(contraption.rows(), contraption.cols());
+  v.fill('.');
+  visited.iter().for_each(|state|{
+    *v.get_mut(state.row, state.col).unwrap() = '#';
+  });
+
+  // print_grid(&v);
+
+  visited.iter().fold(HashSet::new(), |mut acc, s|{
+    acc.insert((s.row, s.col));
+    acc
+  }).len()
+}
+
+fn resolve(state: &State, contraption: &Grid<char>) -> Vec<State> {
+  let c = contraption.get(state.row, state.col).unwrap();
+
+  let mut result = vec![];
+  match (c, state.direction) {
+    ('.', dir) => {
+      // pass right through
+      if let Some(s) = next(state.row, state.col, dir, contraption) {
+        // println!("State:[{state:?}] moved through empty space to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('|', Direction::LEFT|Direction::RIGHT) =>{
+      // split up and down
+      if let Some(s) = next(state.row, state.col, Direction::UP, contraption) {
+        // println!("State:[{state:?}] split on | to: [{s:?}]");
+        result.push(s);
+      }
+      if let Some(s) = next(state.row, state.col, Direction::DOWN, contraption) {
+        // println!("State:[{state:?}] split on | to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('|', Direction::UP|Direction::DOWN) =>{
+      // pass through
+      if let Some(s) = next(state.row, state.col, state.direction, contraption) {
+        // println!("State:[{state:?}] moved through | to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('-', Direction::UP|Direction::DOWN) =>{
+      // split left and right
+      if let Some(s) = next(state.row, state.col, Direction::LEFT, contraption) {
+        // println!("State:[{state:?}] split on - to: [{s:?}]");
+        result.push(s);
+      }
+      if let Some(s) = next(state.row, state.col, Direction::RIGHT, contraption) {
+        // println!("State:[{state:?}] split on - to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('-', Direction::LEFT|Direction::RIGHT) =>{
+      // pass through
+      if let Some(s) = next(state.row, state.col, state.direction, contraption) {
+        // println!("State:[{state:?}] moved through - to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('/', Direction::UP) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::RIGHT, contraption) {
+        // println!("State:[{state:?}] reflected on / to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('/', Direction::DOWN) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::LEFT, contraption) {
+        // println!("State:[{state:?}] reflected on / to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('/', Direction::RIGHT) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::UP, contraption) {
+        // println!("State:[{state:?}] reflected on / to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('/', Direction::LEFT) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::DOWN, contraption) {
+        // println!("State:[{state:?}] reflected on \\ to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('\\', Direction::UP) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::LEFT, contraption) {
+        // println!("State:[{state:?}] reflected on \\ to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('\\', Direction::DOWN) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::RIGHT, contraption) {
+        // println!("State:[{state:?}] reflected on \\ to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('\\', Direction::RIGHT) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::DOWN, contraption) {
+        // println!("State:[{state:?}] reflected on \\ to: [{s:?}]");
+        result.push(s);
+      }
+    },
+    ('\\', Direction::LEFT) =>{
+      // reflect up
+      if let Some(s) = next(state.row, state.col, Direction::UP, contraption) {
+        // println!("State:[{state:?}] reflected on / to: [{s:?}]");
+        result.push(s);
+      }
+    }
+    _ => {}
+  }
+  result
+}
+
+fn next(row: usize, col: usize, dir: Direction, contraption: &Grid<char>) -> Option<State> {
+  match dir {
+    Direction::UP => {
+      if row > 0 {
+        Some(State {
+          row: row - 1,
+          col,
+          direction: dir,
+        })
+      } else {
+        None
+      }
+    }
+    Direction::DOWN => {
+      if row + 1 < contraption.rows() {
+        Some(State {
+          row: row + 1,
+          col,
+          direction: dir,
+        })
+      } else {
+        None
+      }
+    }
+    Direction::LEFT => {
+      if col > 0 {
+        Some(State {
+          row,
+          col: col - 1,
+          direction: dir,
+        })
+      } else {
+        None
+      }
+    }
+    Direction::RIGHT => {
+      if col + 1 < contraption.cols() {
+        Some(State {
+          row,
+          col: col + 1,
+          direction: dir,
+        })
+      } else {
+        None
+      }
+    }
+  }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct State {
+  row: usize,
+  col: usize,
+  direction: Direction,
+}
+
+fn contraption(file: &String) -> Grid<char> {
+  let mut grid = grid![];
+  file.lines().for_each(|line| {
+    grid.push_row(line.chars().collect_vec());
+  });
+  grid
 }
---
+++ day17/src/part2.rs
@@ -0,0 +1,178 @@
+use std::collections::BTreeSet;
+use std::ops::Add;
+use std::thread::sleep;
+use std::time::Duration;
+use std::vec;
+
+use grid::grid;
+use grid::Grid;
+use itertools::Itertools;
+
+pub(crate) fn part2(file: &String) {
+  // solve it like a variant of a path finding algorithm
+
+  // have a sorted state stack
+  // sort on minimal potential heat at the end tile
+  // So add the current accumulated heat and the manhattan distance to the end.
+  // pop the top and add the possible future states to the stack.
+
+  // The state consists of a direction, steps in line, row, column
+  // next states are forward, left, right,
+  // or only left and right if steps in line == 3
+  // a step left or right resets the steps in line to 1
+
+  let city_map = city_map(file);
+
+  let mut states: BTreeSet<State> = BTreeSet::new();
+
+  let w = distance_to_end(&city_map, 0, 0);
+  let initial_state_right = State {
+    weight: w,
+    dir: (0, 1),
+    line: 1,
+    row: 0,
+    col: 0,
+    heat_loss: 0,
+  };
+  states.insert(initial_state_right);
+  let initial_state_down = State {
+    weight: w,
+    dir: (1, 0),
+    line: 1,
+    row: 0,
+    col: 0,
+    heat_loss: 0,
+  };
+  states.insert(initial_state_down);
+//1212
+//1214
+  let mut min = i32::MAX;
+  while let Some(state) = states.pop_first() {
+    if state.row == city_map.rows() - 1 && state.col == city_map.cols() - 1 && state.line >= 4 {
+      if state.heat_loss < min {
+        min = state.heat_loss;
+        println!("found new min value: {min}");
+      }
+      continue;
+    }
+    // early exit, we won't reach faster than previous.
+    if state.heat_loss + distance_to_end(&city_map, state.row, state.col) as i32 >= min {
+      // println!("early exit");
+      continue;
+    }
+    // println!("resolved state: {state:?}");
+    let next = resolve(&city_map, state);
+
+    // println!("to {next:?}");
+    // sleep(Duration::from_millis(1000));
+    states.extend(next);
+  }
+
+  println!("{min}")
+}
+
+fn resolve(map: &Grid<char>, state: State) -> Vec<State> {
+  let mut states = vec![];
+  // forward state
+  if state.line < 9 {
+    if let Some(pos) = moove(state.dir, (state.row, state.col), map) {
+      // println!("pos when moving straight: {pos:?}");
+      let hl = map.get(pos.0, pos.1).unwrap().to_digit(10).unwrap();
+      let md = distance_to_end(map, pos.0, pos.1);
+      states.push(State {
+        weight: state.heat_loss as usize + hl as usize + md,
+        heat_loss: state.heat_loss + hl as i32,
+        line: state.line + 1,
+        row: pos.0,
+        col: pos.1,
+        dir: state.dir,
+      })
+    }
+  }
+
+  if state.line >= 4 {
+    // left
+    let left = left(state.dir);
+    if let Some(pos) = moove(left, (state.row, state.col), map) {
+      // println!("pos when moving left: {pos:?}");
+      let hl = map.get(pos.0, pos.1).unwrap().to_digit(10).unwrap();
+      let md = distance_to_end(map, pos.0, pos.1);
+      states.push(State {
+        weight: state.heat_loss as usize + hl as usize + md,
+        heat_loss: state.heat_loss + hl as i32,
+        line: 1,
+        row: pos.0,
+        col: pos.1,
+        dir: left,
+      })
+    }
+
+    // right
+    let right = right(state.dir);
+    if let Some(pos) = moove(right, (state.row, state.col), map) {
+      // println!("pos when moving right: {pos:?}");
+      let hl = map.get(pos.0, pos.1).unwrap().to_digit(10).unwrap();
+      let md = distance_to_end(map, pos.0, pos.1);
+      states.push(State {
+        weight: state.heat_loss as usize + hl as usize + md,
+        heat_loss: state.heat_loss + hl as i32,
+        line: 1,
+        row: pos.0,
+        col: pos.1,
+        dir: right,
+      })
+    }
+  }
+
+  states
+}
+
+// move in direction on grid, (row,col)
+// return none if moving out of the grid.
+fn moove(dir: (i8, i8), from: (usize, usize), map: &Grid<char>) -> Option<(usize, usize)> {
+  // println!("moving from: {from:?} in direction: {dir:?}");
+  if (from.0 as i32 + dir.0 as i32) < 0 {
+    return None;
+  } else if (from.1 as i32 + dir.1 as i32) < 0 {
+    return None;
+  } else if (from.0 as i32 + dir.0 as i32) >= map.rows() as i32 {
+    return None;
+  } else if (from.1 as i32 + dir.1 as i32) >= map.cols() as i32 {
+    return None;
+  } else {
+    return Some((
+      (from.0 as i32 + dir.0 as i32) as usize,
+      (from.1 as i32 + dir.1 as i32) as usize,
+    ));
+  }
+}
+
+fn left(dir: (i8, i8)) -> (i8, i8) {
+  (-dir.1, dir.0)
+}
+
+fn right(dir: (i8, i8)) -> (i8, i8) {
+  (dir.1, -dir.0)
+}
+
+fn distance_to_end(map: &Grid<char>, row: usize, col: usize) -> usize {
+  ((map.cols() - 1) - col) + ((map.rows() - 1) - row)
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct State {
+  weight: usize,
+  heat_loss: i32,
+  line: i8,
+  row: usize,
+  col: usize,
+  dir: (i8, i8),
+}
+
+fn city_map(file: &String) -> Grid<char> {
+  let mut grid = grid![];
+  file.lines().for_each(|line| {
+    grid.push_row(line.chars().collect_vec());
+  });
+  grid
+}
